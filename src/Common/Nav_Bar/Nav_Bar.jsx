238397331cf31656f7eb65e1252757d662be082d
Rumanito12 <ady_9697@yahoo.com>
2024-02-27 16:56:50 +0100

    ADD & UPDATE : se crea y se mejora el diseño de NavBar

--- src/Common/Nav_Bar/Nav_Bar.jsx
+++ src/Common/Nav_Bar/Nav_Bar.jsx
@@ -1,5 +1,44 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+import "./Nav_Bar.scss"
+
 export const NavBar = () => {
+    let nameProfile = "nombre"
+  return (
     <>
-     <h1>Nav Bar</h1>
+      <Container
+        fluid
+        className="text-center design_navbar"
+      >
+        <Row className="d-flex justify-content-center align-items-center">
+          <Col>
+            <h1>IMG</h1>
+          </Col>
+          <Col>
+            <Row>
+              <Col>
+                <h1>2</h1>
+              </Col>
+              <Col>
+                <h1>3</h1>
+              </Col>
+              <Col>
+                <h1>4</h1>
+              </Col>
+            </Row>
+          </Col>
+          <Col>
+            <DropdownButton title={nameProfile} id="bg-nested-dropdown" className="design_dropDownButton">
+              <Dropdown.Item eventKey="1">Profile</Dropdown.Item>
+              <hr />
+              <Dropdown.Item eventKey="2">Log Out</Dropdown.Item>
+            </DropdownButton>
+          </Col>
+        </Row>
+      </Container>
     </>
-}
+  );
+};
