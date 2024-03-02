91d0d1ef65a49d23da16c424afea5db2e8d9acf4
Rumanito12 <ady_9697@yahoo.com>
2024-03-02 14:13:01 +0100

    UPDATE: pequeña actualización de un fallo de visualización

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,21 +1,43 @@
 import Container from "react-bootstrap/Container";
-import { useSelector } from "react-redux";
-import { userDate } from "../userSlice";
-import { useNavigate } from "react-router-dom";
-import "./Home.scss"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./Home.scss";
 
 export const Home = () => {
   return (
-    <Container id="Main" fluid className="text-center background-warning Container_Body">
-      <h1>Home</h1>
-      <p>
-        Bienvenido a mi portofolio, dentro de no mucho subiré
-        todos mis proyectos deployados tanto en github como Vercel.
-      </p>
-      <p>
-        Y con el backend subido en la plataforma de Amazon Web Services a traves
-        de una maquina virtual Ubuntu.
-      </p>
+    <Container
+      id="Main"
+      fluid
+      className="text-center background-warning design_home"
+    >
+      <Row>
+        <Col>
+          <h1>Home</h1>
+        </Col>
+      </Row>
+      <hr />
+      <Row className="d-flex">  
+        <Col className="border_lateral">
+          <p>
+            Bienvenido a mi portofolio, dentro de no mucho subiré todos mis
+            proyectos deployados tanto en github como Vercel.
+          </p>
+        </Col>
+        <Col>
+          <p>
+            Y con el backend subido en la plataforma de Amazon Web Services a
+            traves de una maquina virtual Ubuntu.
+          </p>
+        </Col>
+      </Row>
+      <hr />
+      <Row>
+        <Col>
+          <p>
+            En la barra de navegación hay diferentes enlaces , al perfil de usuario , proyectos , una parte de contacto y un login personalizado para poder subir diferentes archivos sin tener la necesidad de abrir el codigo cada vez que se va incorporar un nuveo proyecto a la plantilla o para cambiar algunos detalles en el perfil de usuario.
+          </p>
+        </Col>
+      </Row>
     </Container>
   );
 };
