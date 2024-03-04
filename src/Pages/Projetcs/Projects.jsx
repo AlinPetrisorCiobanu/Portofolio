6941415e976e6c0dadfd9a1d88e1dc8afc53da0c
Rumanito12 <ady_9697@yahoo.com>
2024-03-04 22:40:05 +0100

    ADD & UPDATE : se añane texto alternativo a los proyectos y titulo , se mejora el footer.

--- src/Pages/Projetcs/Projects.jsx
+++ src/Pages/Projetcs/Projects.jsx
@@ -11,23 +11,66 @@ export const Projects = () => {
           <h1>Proyectos</h1>
         </Col>
       </Row>
+      <hr />
       <Row className="container_img">
-
-        
+        <div className="image-container">
           <img src="https://assets.asana.biz/transform/d2ffb5c8-a7c2-4e39-8447-f8e2501c5bdc/article-project-planning-project-design-2x" alt="project" />
+          <p>Titulo del Proyecto 1</p>
+        </div>
+        <div className="image-container">
           <img src="https://assets.asana.biz/transform/d2ffb5c8-a7c2-4e39-8447-f8e2501c5bdc/article-project-planning-project-design-2x" alt="project" />
+          <p>Titulo del Proyecto 2</p>
+        </div>
+        <div className="image-container">
           <img src="https://assets.asana.biz/transform/d2ffb5c8-a7c2-4e39-8447-f8e2501c5bdc/article-project-planning-project-design-2x" alt="project" />
+          <p>Titulo del Proyecto 3</p>
+        </div>
+        <div className="image-container">
           <img src="https://assets.asana.biz/transform/d2ffb5c8-a7c2-4e39-8447-f8e2501c5bdc/article-project-planning-project-design-2x" alt="project" />
+          <p>Titulo del Proyecto 4</p>
+        </div>
+        <div className="image-container">
           <img src="https://assets.asana.biz/transform/d2ffb5c8-a7c2-4e39-8447-f8e2501c5bdc/article-project-planning-project-design-2x" alt="project" />
+          <p>Titulo del Proyecto 5</p>
+        </div>
+        <div className="image-container">
           <img src="https://assets.asana.biz/transform/d2ffb5c8-a7c2-4e39-8447-f8e2501c5bdc/article-project-planning-project-design-2x" alt="project" />
+          <p>Titulo del Proyecto 6</p>
+        </div>
+        <div className="image-container">
           <img src="https://assets.asana.biz/transform/d2ffb5c8-a7c2-4e39-8447-f8e2501c5bdc/article-project-planning-project-design-2x" alt="project" />
+          <p>Titulo del Proyecto 7</p>
+        </div>
+        <div className="image-container">
           <img src="https://assets.asana.biz/transform/d2ffb5c8-a7c2-4e39-8447-f8e2501c5bdc/article-project-planning-project-design-2x" alt="project" />
+          <p>Titulo del Proyecto 8</p>
+        </div>
+        <div className="image-container">
           <img src="https://assets.asana.biz/transform/d2ffb5c8-a7c2-4e39-8447-f8e2501c5bdc/article-project-planning-project-design-2x" alt="project" />
+          <p>Titulo del Proyecto 9</p>
+        </div>
+        <div className="image-container">
           <img src="https://assets.asana.biz/transform/d2ffb5c8-a7c2-4e39-8447-f8e2501c5bdc/article-project-planning-project-design-2x" alt="project" />
+          <p>Titulo del Proyecto 10</p>
+        </div>
+        <div className="image-container">
           <img src="https://assets.asana.biz/transform/d2ffb5c8-a7c2-4e39-8447-f8e2501c5bdc/article-project-planning-project-design-2x" alt="project" />
+          <p>Titulo del Proyecto 11</p>
+        </div>
+        <div className="image-container">
           <img src="https://assets.asana.biz/transform/d2ffb5c8-a7c2-4e39-8447-f8e2501c5bdc/article-project-planning-project-design-2x" alt="project" />
-
-      </Row>
+          <p>Titulo del Proyecto 12</p>
+        </div>
+        <div className="image-container">
+          <img src="https://assets.asana.biz/transform/d2ffb5c8-a7c2-4e39-8447-f8e2501c5bdc/article-project-planning-project-design-2x" alt="project" />
+          <p>Titulo del Proyecto 13</p>
+        </div>
+        <div className="image-container">
+          <img src="https://assets.asana.biz/transform/d2ffb5c8-a7c2-4e39-8447-f8e2501c5bdc/article-project-planning-project-design-2x" alt="project" />
+          <p>Titulo del Proyecto 14</p>
+        </div>
+    </Row>
+    <hr />
     </Container>
   );
 };
