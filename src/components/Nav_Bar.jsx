543b1dd1137e7108ac3933c1e3222eaad3aeba99
AlinPC <alinpetrisorciobanu@outlook.es>
2025-12-08 22:56:38 +0100

    ADD: se añade menu de hamburguesa para la vista de moviles y tabletas.

--- src/components/Nav_Bar.jsx
+++ src/components/Nav_Bar.jsx
@@ -1,7 +1,9 @@
 import {useNavigate} from "react-router-dom"
+import { useState } from "react";
 
 const Nav_Bar = () => {
     const navigate = useNavigate();
+    const [open, setOpen] = useState(false);
 
     const go_to_home = () => {
         navigate("/")
@@ -18,13 +20,16 @@ const Nav_Bar = () => {
     const go_to_contacto = () => {
         navigate("/contacto")
     }
+    const hamburguer_menu = () => {
+        setOpen(!open)
+    }
 
     return(
         <div className="container_nav_bar">
-            <div className="nav_bar_first_option">
-                <div className="">a</div>
+            <div className="nav_bar_hamburguer">
+                <div className="hamburguer" onClick={hamburguer_menu}>☰</div>
             </div>
-            <div className="nav_bar_menu">
+            <div className={open ? "nav_bar_menu opened" : "nav_bar_menu" }>
                 <div className="option" onClick={go_to_home}>Inicio</div>
                 <div className="option" onClick={go_to_project}>Proyectos</div>
                 <div className="option" onClick={go_to_curriculum}>Curriculum</div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,15 @@
 import Nav_Bar from "../components/Nav_Bar";
 import "./../styles/container.css"
 import "./../styles/nav_bar_design.css"
+import "./../styles/nav_bar_media_querry.css"
 
 const Home = () => {
     return (
         <div className="container">
             <Nav_Bar />
-           <h1>Mi nuevo proiecto de portofolio</h1>
+            <main>
+                <h1>Mi nuevo proiecto de portofolio</h1>
+            </main>
         </div>
     )
 
