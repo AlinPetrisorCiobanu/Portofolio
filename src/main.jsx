896af2cfa80f78e049348da6391df1b8e698c1f6
Alin <ady_9697@yahoo.com>
2026-03-03 23:40:08 +0100

    PATCH: se cambia los nombres de los archivos css por fallo de deployments en vercel.

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./assets/styles/Global.css"
-import "./assets/styles/Container.css"
+import "./assets/styles/container.css"
 import "./assets/styles/Nav_bar.css"
 import "./assets/styles/Nav_bar_responsive.css"
-import "./assets/styles/Container_proyecto.css"
+import "./assets/styles/container_proyecto.css"
 import "./assets/styles/Home.css"
 import "./assets/styles/Projects.css"
 import "./assets/styles/Projects_responsive.css"
