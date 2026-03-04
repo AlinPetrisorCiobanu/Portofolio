e2cf8f82912697985a5e78bc03bcff814a8cf3e5
Alin <ady_9697@yahoo.com>
2026-03-04 15:09:22 +0100

    ADD & UPDATE: se modifica el archivo princiapl de los proyectos, cada proyecto aparece en forma de carpeta.

--- src/components/Project_box.jsx
+++ src/components/Project_box.jsx
@@ -6,10 +6,17 @@ const Project_box = ({ id, name, detail ,images}) => {
         navigate(`/project/${id}`);
     }
     return (
-        <div className="box_project" onClick={()=>go_to(id)}>
-            <h2>{name} : Proyecto número {id}</h2>
-            <img src={images[0]} alt={name}/>
-            <h2>{detail}</h2>
+        <div className="box_project" onClick={()=>{go_to(id)}}>
+            <div className="folder_tab"></div>
+
+            <div className="folder_body">
+                <img src={images[0]} alt={name} />
+
+                <div className="project_content">
+                    <h3>{name}</h3>
+                    <p>{detail}</p>
+                </div>
+            </div>
         </div>
     )
 
