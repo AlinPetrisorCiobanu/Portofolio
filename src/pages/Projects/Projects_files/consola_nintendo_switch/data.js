954a32aebb7aa9ff4fbff7578a132501daaf173c
Alin <ady_9697@yahoo.com>
2026-03-04 18:45:26 +0100

    ADD & UPDATE: se añaden varios proyectos y se mejora la escritura.

---
+++ src/pages/Projects/Projects_files/consola_nintendo_switch/data.js
@@ -0,0 +1,16 @@
+import img_01 from "./A.png";
+import img_02 from "./consola.png";
+import img_03 from "./info.png";
+import img_04 from "./option1.png";
+import img_05 from "./option2.png";
+
+export default {
+    id:2,
+    name:"Nintendo Switch",
+    detail:"Frontend Restaurante la bunica",
+    description:"Es una aplicación que esta desarollada con REACT y deployada en Vercel, una página web simple y compleja.",
+    link_github : "https://github.com/AlinPetrisorCiobanu/Restaurante_La_Bunica",
+    link_deploymen : "https://restaurante-la-bunica.vercel.app/",
+    estado : "Finalizado",
+    images: [img_01,img_02,img_03,img_04,img_05]
+};
---
+++ src/pages/Projects/Projects_files/restaurante_la_bunica/data.js
@@ -0,0 +1,14 @@
+import img_01 from "./1.png";
+import img_02 from "./2.png";
+import img_03 from "./3.png";
+import img_04 from "./B.png";
+export default {
+    id:1,
+    name:"Restaurante La Bunica",
+    detail:"Frontend Restaurante la bunica",
+    description:"Es una aplicación que esta desarollada con REACT y deployada en Vercel, una página web simple y compleja.",
+    link_github : "https://github.com/AlinPetrisorCiobanu/Restaurante_La_Bunica",
+    link_deploymen : "https://restaurante-la-bunica.vercel.app/",
+    estado : "Finalizado",
+    images: [img_01,img_02,img_03,img_04]
+};
